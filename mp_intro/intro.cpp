a6cabb2a81b91f4471f36017015a8bbae635b23d
Xinshuo <xinshuo3@illinois.edu>
2020-09-06 16:52:42 -0500

    finish part 2: rotate an image

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -6,6 +6,19 @@
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
+  cs225::PNG inputPNG;
+  inputPNG.readFromFile(inputFile);
+  unsigned width = inputPNG.width();
+  unsigned height = inputPNG.height();
+  cs225::PNG outputPNG(width, height);
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      cs225::HSLAPixel & inputPixel = inputPNG.getPixel(x, y);
+      cs225::HSLAPixel & outputPixel = outputPNG.getPixel(width -1 - x, height - 1 - y);
+      outputPixel = inputPixel;
+    }
+  }
+  outputPNG.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
