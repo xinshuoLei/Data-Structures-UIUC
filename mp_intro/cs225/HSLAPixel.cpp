79392cec1b935b4e471e689dd4962b4b237a7644
Xinshuo <xinshuo3@illinois.edu>
2020-09-06 16:23:42 -0500

    finish Makefile. Add HSLAPixel.cpp and HSLAPixel.h from lab_intro

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -3,9 +3,62 @@
  * Implementation of the HSLAPixel class for use in with the PNG library.
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #include "HSLAPixel.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
 
 namespace cs225 {
+    // defalt pixel. non-transparent and white
+    HSLAPixel::HSLAPixel(){
+        h = 0.0;
+        s = 0.0;
+        l = 1.0;
+        a = 1.0;
+    }
+    // opaque pixel with given hue, saturation and luminance
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
+        a = 1.0;
+        if (hue >= 360 || hue < 0) {
+            h = 0.0;
+        } else {
+            h = hue;
+        }
+        if (saturation > 1 || saturation < 0) {
+            s = 0.0;
+        } else {
+            s = saturation;
+        }
+        if (luminance > 1 || luminance < 0) {
+            l = 1.0;
+        } else {
+            l = luminance;
+        }
+    }
+    // pixel with given hue, saturation, luminance and alpha
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) {
+        if (hue >= 360 || hue < 0) {
+            h = 0.0;
+        } else {
+            h = hue;
+        }
+        if (saturation > 1 || saturation < 0) {
+            s = 0.0;
+        } else {
+            s = saturation;
+        }
+        if (luminance > 1 || luminance < 0) {
+            l = 1.0;
+        } else {
+            l = luminance;
+        }
+        if (alpha > 1 || alpha < 0) {
+            a = 1.0;
+        } else {
+            a = alpha;
+        }
+    }
 }
--- mp_intro/cs225/HSLAPixel.h
+++ mp_intro/cs225/HSLAPixel.h
@@ -2,9 +2,27 @@
  * @file HSLAPixel.h
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #pragma once
 
+#include <iostream>
+#include <sstream>
+
 namespace cs225 {
+    class HSLAPixel {
+      public:
+        double h;   // hue of the pixel
+        double s;   // saturation of the pixel
+        double l;   // luminance of the pixel
+        double a;   // alpha of the pixel
+
+        // create a default HSLAPixel
+        HSLAPixel();
+        // create a opaque HSLAPixel with the given hue, saturation and luminance
+        HSLAPixel(double hue, double saturation, double luminance);
+        // create HSLAPixel with the given hue, saturation, luminance, and alpha
+        HSLAPixel(double hue, double saturation, double luminabce, double alpha);
+    };
 }
