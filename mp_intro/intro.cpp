9a5835351011b6ab8240f3e9ce1eb35bb2269d1f
Xinshuo <xinshuo3@illinois.edu>
2020-09-06 21:54:30 -0500

    finish mp_intro

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -2,6 +2,7 @@
 #include "cs225/HSLAPixel.h"
 
 #include <string>
+#include <cmath>
 
 
 void rotate(std::string inputFile, std::string outputFile) {
@@ -24,6 +25,42 @@ void rotate(std::string inputFile, std::string outputFile) {
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
+  double diagonal_distance = sqrt((width - 1) * (width - 1) + (height - 1) * (height - 1));
+  for (unsigned a = 0; a < width; a++) {
+    for (unsigned b = 0; b < width; b++) {
+      cs225::HSLAPixel & pixel = png.getPixel(a, b);
+      pixel.s = 1.0;
+      double distance = sqrt(a * a + b * b);
+      // change hue
+      if (distance <= (diagonal_distance / 4)) {
+        pixel.h = 60;
+      } else if (distance <= (diagonal_distance / 2)) {
+        pixel.h = 120;
+      } else if (distance <= (diagonal_distance * 0.75)) {
+        pixel.h = 180;
+      } else {
+        pixel.h = 210;
+      }
+      // change luminance
+      if (distance <= (diagonal_distance) * 0.125) {
+        pixel.l = 0.8;
+      } else if (distance <= (diagonal_distance * 0.25)) {
+        pixel.l = 0.7;
+      } else if (distance <= (diagonal_distance * 0.375)) {
+        pixel.l = 0.65;
+      } else if (distance <= (diagonal_distance * 0.5)) {
+        pixel.l = 0.55;
+      } else if (distance <= (diagonal_distance * 0.625)) {
+        pixel.l = 0.5;
+      } else if (distance <= (diagonal_distance * 0.75)) {
+        pixel.l = 0.45;
+      } else if (distance <= (diagonal_distance * 0.875)) {
+        pixel.l = 0.4;
+      } else {
+        pixel.l = 0.35;
+      }
+    }
+  }
 
   return png;
 }
