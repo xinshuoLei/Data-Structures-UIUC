4f74bcbbbe55aeca50338531d34415fc6269d068
Xinshuo <xinshuo3@illinois.edu>
2020-09-08 11:33:01 -0500

    finish lab_debug

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -5,6 +5,7 @@
 #pragma GCC diagnostic ignored "-Wuninitialized"
 #include <cstdlib>
 #include <cmath>
+#include <iostream>
 
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
@@ -28,30 +29,27 @@ PNG* setupOutput(unsigned w, unsigned h) {
 
  * @return a pointer to the color to use when sketchifying
  */
-HSLAPixel* myFavoriteColor() {
-    HSLAPixel p(-1, 0.8, 0.5);
-    return &p;
+HSLAPixel myFavoriteColor() {
+    const HSLAPixel p (200, 0.8, 0.5);
+    return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
+    PNG* original = new PNG; // change = null to a memory address
     original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
-
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor();
+    HSLAPixel myPixel = myFavoriteColor();
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; y < height; y++) {
+        for (unsigned x = 1; x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
@@ -59,9 +57,9 @@ void sketchify(std::string inputFile, std::string outputFile) {
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel& currOutPixel = (*output).getPixel(x, y);
             if (diff > 20) {
-                currOutPixel = *myPixel;
+                currOutPixel = myPixel;
             }
         }
     }
@@ -70,7 +68,6 @@ void sketchify(std::string inputFile, std::string outputFile) {
     output->writeToFile(outputFile);
 
     // Clean up memory
-    delete myPixel;
     delete output;
     delete original;
 }
