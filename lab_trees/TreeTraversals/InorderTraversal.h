24eefe841a2e14379512e07b83951d281cc65557
Xinshuo <xinshuo3@illinois.edu>
2020-10-02 13:38:23 -0500

    finish InorderTraversal.h

--- lab_trees/TreeTraversals/InorderTraversal.h
+++ lab_trees/TreeTraversals/InorderTraversal.h
@@ -25,7 +25,11 @@ class InorderTraversal : public TreeTraversal<T> {
     InorderTraversal(typename BinaryTree<T>::Node* root)
 	    : root(root)
     {
-      stack.push(root);	
+      typename BinaryTree<T>::Node* current = root;
+      while(current!= NULL) {
+        stack.push(current);
+        current = current -> left;
+      }
       // your code here
     }
 
@@ -61,6 +65,14 @@ class InorderTraversal : public TreeTraversal<T> {
      */	
     void add(typename BinaryTree<T>::Node *& treeNode) {
       // your code here
+      if (treeNode -> right == NULL) {
+        return;
+      }
+      typename BinaryTree<T>::Node* current = treeNode -> right;
+      while (current != NULL) {
+        stack.push(current);
+        current = current -> left;
+      }
       return;	
     }
 
