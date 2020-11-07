716150359f702c74e9ea75555a595aa9709fde0e
Xinshuo Lei <xinshuo3@illinois.edu>
2020-11-06 22:34:33 -0600

    prepare part2 for autograder

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -1,1 +1,26 @@
 /* Your code here! */
+#include "maze.h"
+
+void SquareMaze::makeMaze(int width, int height) { 
+    // nothing
+}
+
+bool SquareMaze::canTravel(int x, int y, int dir) const {
+    return false;
+}
+
+void SquareMaze::setWall(int x, int y, int dir, bool exists) {
+    // nothing
+}
+
+std::vector<int> SquareMaze::solveMaze() {
+    return std::vector<int>();
+}
+
+cs225::PNG* SquareMaze::drawMaze() const {
+    return NULL;
+}
+
+cs225::PNG* SquareMaze::drawMazeWithSolution() {
+    return NULL;
+}
--- mp_mazes/maze.h
+++ mp_mazes/maze.h
@@ -1,1 +1,15 @@
 /* Your code here! */
+#pragma once
+#include <vector>
+#include "maze.h"
+#include "cs225/PNG.h"
+
+class SquareMaze {
+    public:
+        void makeMaze(int width, int height);
+        bool canTravel(int x, int y, int dir) const;
+        void setWall(int x, int y, int dir, bool exists);
+        std::vector<int> solveMaze();
+        cs225::PNG* drawMaze() const;
+        cs225::PNG* drawMazeWithSolution();
+};
